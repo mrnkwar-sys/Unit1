9527df058c9847507f11d2c333fdb4ea9705bfc9
mr.nkwar <mr.nkwar@106-18.info2>
2025-09-22 10:22:02 +0200

    Exercises 3 and 2

--- src/unit1/Exercise02.java
+++ src/unit1/Exercise02.java
@@ -1,9 +1,30 @@
 package unit1;
 
+import java.util.Scanner;
+
 public class Exercise02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// create a new Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//declare the age variable
+		int age;
+		int b;
+		
+		//Print the value in the console
+		System.out.println("Introduzca su edad:");
+		
+		//The console waits for us to introduce the age
+		age = sc.nextInt();
+		b = age+1;
+		
+		//The previous value is shown but +1
+		System.out.println("Su edad el año que viene será:" + b);
+		
+		//Close Scanner
+		sc.close();
+		
 
 	}
 
