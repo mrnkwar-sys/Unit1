95cf8b42620cfb21199ca745165ca3f948bdd628
mr.nkwar <mr.nkwar@106-18.info2>
2025-09-22 09:38:52 +0200

    Unit 1

---
+++ src/unit1/Exercise01.java
@@ -0,0 +1,27 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// create a new Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// declare the numero variable
+		int numero;
+
+		// Print the value in the console
+		System.out.print("Introduzca un número:");
+
+		// The console waits for us to introduce the number
+		numero = sc.nextInt();
+		
+		//Show the value in the console
+		System.out.println("Ha escrito:" + numero);
+
+		// Close Scanner
+		sc.close();
+
+	}
+}
