e444ec0f82fe045a67828d341a43459d86289e7f
mr.nkwar <mr.nkwar@106-18.info2>
2025-09-29 10:22:35 +0200

    29/9/25

---
+++ src/parte2/Exercise03.java
@@ -0,0 +1,21 @@
+package parte2;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		//Create the Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//The first number introduced
+		int number1;
+		
+		//The second number introduced
+		int number2;
+		
+		//The number that we need to sum to num1 
+
+	}
+
+}
